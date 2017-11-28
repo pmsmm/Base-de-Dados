a133fc9fc8de06d0617864c24ea03fbc05d0639a
Mike_g <25959578+migueldgoncalves@users.noreply.github.com>
2017-11-28 17:05:05 +0000

    Alinea e) a funcionar

---
+++ Site_BD/AlineaE1.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <h3>Lista de sub-categorias da categoria <?=$_REQUEST['super_categoria']?></h3>
+        <form action="AlineaE2.php" method="post">
+            <p>Nome da categoria: <input type="text" name="super_categoria"/></p>
+            <p><input type="submit" value="Submit"/></p>
+        </form>
+    </body>
+</html>
