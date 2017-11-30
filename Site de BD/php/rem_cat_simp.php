2a5fa0d95c324401fd690b9a0defd5df812fef93
Pedro Maria <23109237+pmsmm@users.noreply.github.com>
2017-11-30 10:21:25 +0000

    Teste da remoção das super categorias e categorias simples

--- Site de BD/php/rem_cat_simp.php
+++ Site de BD/php/rem_cat_simp.php
@@ -7,6 +7,33 @@
 	}
 
 	try{
+	    $var1;
+
+	    $teste=$db->prepare("SELECT super_categoria FROM public.constituida WHERE categoria = (:pesquisa);");
+
+	    $teste->bindParam(':pesquisa', $_POST['rem_cat_simples'], PDO::PARAM_STR);
+
+	    $teste->execute();
+
+	    $teste->fetchAll();
+	    foreach ($teste as $row){
+	        $var1=$row['super_categoria'];
+        }
+
+        $bora=$db->prepare("SELECT categoria FROM public.constituida WHERE supercategoria = (:finalmente);");
+        $bora->bindParam(':finalmente', $var1, PDO::PARAM_STR);
+        $bora->execute();
+
+        $result=$bora->rowCount();
+        if($result = 1){
+            echo "Não é possível remover a categoria pretendida pois é a única da supercategoria "+$var1;
+            close();
+        }
+    }catch (PDOException $e){
+	    handle_sql_errors($e);
+    }
+
+	try{
 		$prepared = $db->prepare("DELETE FROM public.categoria WHERE nome = (:categoria_simp);");
 	
 		$prepared->bindParam(':categoria_simp', $_POST['rem_cat_simples'], PDO::PARAM_STR);
--- Site de BD/php/rem_sup_cat.php
+++ Site de BD/php/rem_sup_cat.php
@@ -6,7 +6,35 @@
 		die();
 	}
 
-	try{
+    try{
+        $var1;
+
+        $teste=$db->prepare("SELECT super_categoria FROM public.constituida WHERE categoria = (:pesquisa);");
+
+        $teste->bindParam(':pesquisa', $_POST['rem_cat'], PDO::PARAM_STR);
+
+        $teste->execute();
+
+        $teste->fetchAll();
+        foreach ($teste as $row){
+            $var1=$row['super_categoria'];
+        }
+
+        $bora=$db->prepare("SELECT categoria FROM public.constituida WHERE supercategoria = (:finalmente);");
+        $bora->bindParam(':finalmente', $var1, PDO::PARAM_STR);
+        $bora->execute();
+
+        $result=$bora->rowCount();
+        if($result = 1){
+            echo "Não é possível remover a super categoria pretendida pois é a única da supercategoria "+$var1;
+            close();
+        }
+    }catch (PDOException $e){
+        handle_sql_errors($e);
+    }
+
+
+    try{
 		$prepared = $db->prepare("DELETE FROM public.categoria WHERE nome = (:categoria_rem);");
 	
 		$prepared->bindParam(':categoria_rem', $_POST['rem_cat'], PDO::PARAM_STR);
