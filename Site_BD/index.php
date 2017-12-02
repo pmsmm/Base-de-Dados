e7ec8c78a617849847f4f85d85c5af16073ce2f1
Mike_g <25959578+migueldgoncalves@users.noreply.github.com>
2017-12-02 03:34:30 +0000

    Index com espaço debaixo do botão

--- Site_BD/index.php
+++ Site_BD/index.php
@@ -12,8 +12,8 @@
 </head>
 <body style="background-image: url(Teste.jpg);">
 	<div class="jumbotron" style="text-align: center; background: transparent;">
-		<h1>Bem Vindos ao Market Manager</h1>
-		<p style="color: white;">Bem vindos, preparem-se para a melhor experiência de gestão de supermercados de sempre!</p>
+		<h1>Bem Vindo ao Market Manager</h1>
+		<p style="color: white;">Bem vindo, prepara-se para a melhor experiência de gestão de supermercados de sempre!</p>
 	</div>
 
 	<?php
@@ -29,19 +29,20 @@
 
 			<div class="col-lg-4" style="text-align: center;">
 
-				<h1>Adicionar categorias e subcategorias </h1>
+				
+				<h1 style= "color: white;">Adicionar categorias e subcategorias </h1>
 				<form action="add_cat_e_sub.php" method = "post" style="display: inline-block; margin-top;">
 					<input type="text" id="categoria" name="supercategoria" placeholder="Nome da Super-Categoria a Adicionar..." style="width: 270px;">
 					<p><input type="text" placeholder="Nome da Categoria-Simples a Adicionar..." id="sub_categoria" name="categoria" style="width: 270px;"></p>
-					<p><input type="submit" value="Submit"/></p>
+					<p><input type="submit" value="Adicionar"/></p>
 					<p>
 				</form>
 
 			
-				<p>Remover categoria/subcategoria </p>
-				<form action="rmCategoria.php" method = "post" style="display: inline-block; margin-top: 15px;">
+				<h1 style= "color: white;">Remover categoria/subcategoria </h1>
+				<form action="rm_cat_e_sub.php" method = "post" style="display: inline-block; margin-top: 15px;">
 					<p><input type="text" name="rm_categoria" placeholder="Nome da categoria a remover ..." id="parametro" style="width: 270px;"></p>
-					<button>Enter</button>
+					<button>Remover</button>
 				</form>
 
 				<br>
@@ -49,12 +50,16 @@
 
 				<div style="margin-top: 20px;">
 
-					<h6>Alterar designacao produto </h6>
+					<h1 style= "color: white;">Alterar designacao de um produto </h1>
 					<form style="display: inline-block;" action="alter_design.php" method="post">
 						<input type="number" name="ean" placeholder="Insira o EAN do Produto a Alterar:" style="margin-top: 15px;" id="ean_id">
 						<input type="text" name="ean_design" placeholder="Insira a Nova Designação:" style="margin-top: 15px;" id="design_id">
-						<button>Enter</button>
+						<button>Alterar</button>
 					</form>
+
+					<br>
+					<br>
+					<br>
 
 					<div>
 						<div style="border: solid; border-color: black; background-color: red; margin-top: 148px; margin-right: 30px; display: none;" id="cai_temp_bad">
@@ -70,15 +75,16 @@
 
 				<div style="margin-top: 20px;">
 
-					<h1>Listar eventos reposicao </h1>
+					<h1 style= "color: white;">Listar eventos de reposicao</h1>
 					<form action="listar_repo.php" method="post" style="display: inline-flex;">					
-						<input type="text" name="ean" placeholder="Inserir o Nome do Produto a Listar:">
-						<button>Enter</button>
+						<input type="text" name="ean" placeholder="Inserir o Ean do Produto a Listar:">
+						<button>Listar</button>
+						
 					</form>
-					<h4>Listar subcatgeorias de superCategoria</h4>
+					<h1 style= "color: white;">Listar subcategorias </h1>
 					<form action="listar_cats.php" method="post" style="display: inline-flex; margin-top: 20px;">					
 						<input type="text" name="super_categoria" placeholder="Inserir o Nome da Super-Categoria a Listar:">
-						<button>Enter</button>
+						<button>Listar</button>
 					</form>
 
 
@@ -87,8 +93,8 @@
 			</div>
 
 			<div class="col-lg-4">
-				<h2>Adicionar produto</h2>
-				<form action="produto_ins.php" method="post" style="display: inline-block;">
+				<h1 style= "color: white;">Adicionar produto </h1>
+				<form action="produto_ins_parse.php" method="post" style="display: inline-block;">
 					<input type="number" id="campo1" name="ean_ins" placeholder="Insira o EAN do Produto:">
 					<br>
 					<input type="text" id="campo2" name="design_ins" placeholder="Insira a Designação do Produto:">
@@ -106,10 +112,10 @@
 						<!--<button style="margin-left: 10px;">Remover</button>-->
 					</div>
 				</form>
-				<h3>Remover produto</h3>
+				<h1 style= "color: white;">Remover produto </h1>
        			<form action="produto_rem.php" method="post">
             		<input type="number" name="ean" placeholder="Insira o ean:">
-            		<br><input type="submit" value="Submit"/></br>
+            		<br><input type="submit" value="Remover"/></br>
         		</form>
 			</div>
 		</div>
